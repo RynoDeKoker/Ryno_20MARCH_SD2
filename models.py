19097a5f6b991e7573bc28895a9a6cf01c4c3663
LisaTerblanche <lisaterblanche0@gmail.com>
2026-03-20 11:36:15 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,13 +1,31 @@
-#Class 1
+# models.py
+
+class User:
+    def __init__(self, user_id, username, password, role):
+        self.user_id = user_id
+        self.username = username
+        self.password = password
+        self.role = role
 
 
-#Class 2
+class Product:
+    def __init__(self, product_id, name, price, stock_qty):
+        self.product_id = product_id
+        self.name = name
+        self.price = price
+        self.stock_qty = stock_qty
 
 
-#Class 3
+class Order:
+    def __init__(self, order_id, user_id, product_id, quantity):
+        self.order_id = order_id
+        self.user_id = user_id
+        self.product_id = product_id
+        self.quantity = quantity
 
 
-#Class 4
-
-#jldnfsdikjfnsdkjfmnsd kfmsdfkmsdf
-#hkkbkhvkhvkvkhv
+class Warehouse:
+    def __init__(self, warehouse_id, location, capacity):
+        self.warehouse_id = warehouse_id
+        self.location = location
+        self.capacity = capacity
